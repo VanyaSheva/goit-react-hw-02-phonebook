641cd181f968c64296c4db44a34d733915de8d26
Vanya Shevchuk <aggressor2000@gmail.com>
2020-04-16 18:39:43 +0300

    Updates

---
+++ precache-manifest.78bd2d81dbb3b9394b6d7ccdb890af18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d549ee8a565657356d52a2bced55e31b",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "64a8eb955e300cf7c7e6",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5ab8cb59.chunk.css"
+  },
+  {
+    "revision": "cd0f404cd6d62c395658",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b0b3fd8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b0b3fd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a8eb955e300cf7c7e6",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.d7590d4a.chunk.js"
+  },
+  {
+    "revision": "959ec6eab86ecd586fe4",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.9d4c9ebd.js"
+  }
+]);
